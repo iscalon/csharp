51d84406dab0575e8a24c3cbf5f7b3a7afe0b24a
scional <138497576+scional@users.noreply.github.com>
2026-08-01 15:05:45 +0200

    Update

--- MaConsoleApp/MaConsoleApp/Program.cs
+++ MaConsoleApp/MaConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using MaConsoleApp;
 
+// Version : p.82
+
 static class Program
 {
 
@@ -19,7 +21,8 @@ static class Program
         Console.WriteLine($"255 in hex is {byte.MaxValue:X2}"); // X2 = 2-digit hexadecimal
         Console.WriteLine($"77 in hex is {77:X2}"); // X2 = 2-digit hexadecimal
         Console.WriteLine($$"""{ "TimeStamp": "{{DateTime.Now}}" }"""); // 2 symboles $ = il faut interpoler 2 accolades. Va donner : { "TimeStamp": "07/07/2026 16:46:24" }
-        Console.WriteLine(new Panda("Pandi").ToString());
+        Console.WriteLine(new Panda("Pandi", "Panda").ToString());
+        Console.WriteLine(new Panda("Pandi McDonald").ToString());
     }
 }
 
@@ -35,17 +38,60 @@ namespace MaConsoleApp
         }
     }
 
-    public class Panda(String name)
+    public class Panda
     {
+
+        private readonly /* equivalent de final en Java */ string title;
+        private readonly FullName fullName;
+
+
+        public Panda(string fullName) : this(fullName, "") {
+            Console.WriteLine("1er constructeur !"); // Appelé avant d'appeler l'autre constructeur
+        }
+
+
+        public Panda(String firstName, String lastName, String title = "M." /* valeur optionnelle */)
+        {
+            this.title = title;
+            this.fullName = new FullName
+            {
+                firstName = firstName,
+                lastName = lastName
+            };
+        }
+
 
         public String GetName()
         {
-            return name;
+            string fullName = this.fullName.firstName + " " + this.fullName.lastName;
+            Util.Split(fullName, out string fName, out string lName); // Déclaration de fName et lName en même temps que l'appel
+            // Si on se fichait de la 2ème chaine par exemple, on pouvait faire : Util.Split(fullName, out string fName, out _)
+            return this.title + " " + fName + " " + lName;
         }
 
         override public String ToString()
         {
             return GetName();
         }
+
+
+        
     }
+
+    struct FullName { public String firstName; public String lastName; }
+
+    public class Util
+    {
+        public static void Split(string fullName, out string firstName, out string lastName)
+        {
+            string[] parts = fullName.Split(' ');
+            firstName = parts[0];
+            lastName = "";
+            if (parts.Length >= 2)
+            {
+                lastName = parts[1];
+            }
+        }
+    }
+
 }
